9b189546dd0651aabfab72f6cdd9320a6e0a3950
alyssalanae <briggs.l.alyssa@gmail.com>
2020-01-17 14:48:44 -0600

    Moved dao

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -2,7 +2,6 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
-import dao.Config;
 
 import java.sql.*;
 
---
+++ src/main/java/com/codeup/adlister/models/Category.java
@@ -0,0 +1,35 @@
+package com.codeup.adlister.models;
+
+import com.codeup.adlister.dao.Categories;
+
+import java.util.List;
+
+public class Category {
+    private long id;
+    private String name;
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
